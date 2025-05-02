ddc3e6d7229c80e176fcec05b8d38e079eeba72b
Túlio Santo <90075062+tuliosanto@users.noreply.github.com>
2025-05-02 18:47:37 +0000

    Alterando o eu te amo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Karin, eu amo voce. Se vc me ama, dá uma risadinha.</h1>;
+  return <h1>Karin, eu te amo. Se vc me ama, dá uma risadinha.</h1>;
 }
 
 export default Home;
